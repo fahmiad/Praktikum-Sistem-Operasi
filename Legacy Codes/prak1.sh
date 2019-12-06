119becf6612162877f92594b851a68fa5dda2458
Fahmi AD <43086741+fahmiad@users.noreply.github.com>
2019-12-06 14:35:06 +0700

    Create prak1.sh

---
+++ Legacy Codes/prak1.sh
@@ -0,0 +1,49 @@
+mkdir Semester 3
+cd Semester 3
+mkdir {Sisop,Kwu,Pemlan,Statkom,Belneg,Metnum,Pancasila}
+ls
+cd StatKom
+nano desc.txt
+cd ..
+cd SisOp
+nano desc.txt
+cd ..
+cd MetNum
+nano desc.txt
+cd ..
+cd BelNeg
+nano desc.txt
+cd ..
+cd PemLan
+nano desc.txt
+cd ..
+cd KcB
+nano desc.txt
+cd ..
+cd Pancasila
+nano desc.txt
+cd ..
+cd Statkom
+cat desc.txt
+cd ..
+cd SisOp
+cat desc.txt
+cd ..
+cd MetNum
+cat desc.txt
+cd ..
+cd BelNeg
+cat desc.txt
+cd ..
+cd PemLan
+cat desc.txt
+cd ..
+cd KcB
+cat desc.txt
+cd ..
+cd Pancasila
+cat desc.txt
+cd ..
+
+sudo apt-get install vokoscreen
+
